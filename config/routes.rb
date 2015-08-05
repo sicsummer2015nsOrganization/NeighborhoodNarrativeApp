d6d1445a7e053ce8e2c882dfb1e8cc581188d7c5
shaista ansari <shaansari@hotmail.com>
2015-08-04 19:29:35 -0500

    doing search returns school suggestion

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "users/index", :to => "users#index", as: 'users_index'
   get "users/home", :to => "users#home", as: 'users_home'
   get "users/search", :to => "users#search", as: 'users_search'
+ 
   get "users/finalpage", :to => "users#finalpage", as: 'users_finalpage'
   post "users/finalpage"
-
 end
